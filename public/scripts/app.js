1f800cc83e608f4cf01112bfd0d50ad915c80642
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-26 01:26:49 +0000

    add every assignments for w3d3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,74 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+function renderTweets(tweets) {
+  tweets.forEach(function(element) {
+    $("#tweetsArea").prepend(createTweetElement(element));
+  });
+}
 
+function createTweetElement(tweet) {
+  //var $tweet = $('<article>').addClass('Alltweets');
+  let username = tweet.user.handle;
+  let avatar = tweet.user.avatars.small;
+  let name = tweet.user.name
+  let content = tweet.content.text;
+  let time = tweet.created_at;
+  let code = `
+      <article id="allTweets">
+        <header>
+          <img class="avatar" src="${avatar}" >
+            <h3 class="user">${name}</h3>
+        </header>
+        <p>${content}</p>
+        <footer>
+          <p>${time}</p>
+        </footer>
+      </article>
+  `
+  return code;
+}
+
+function displayTweets() {
+  $.ajax('/tweets')
+  .then((data) => {
+    $('#tweetsArea').empty();
+    renderTweets(data);
+  });
+}
+
+function loadTweets(){
+  $.ajax({
+    url: 'http://localhost:8080/tweets',
+    method: "GET"
+  }).then(function(){
+    displayTweets();
+  })
+}
+
+loadTweets();
+
+$(function() {
+  $('form').on('submit', function(ev){
+    ev.preventDefault();
+    if ($('textarea')[0].value === "") {
+      alert("You can't tweet empty tweet");
+    } else if ($('textarea')[0].textLength > 140) {
+      alert("You can't tweet more than 140 characters");
+    } else {
+      let dataTweets = $(this).serialize();
+      $.ajax('/tweets', {method: "post", data: dataTweets})
+      .then((result) => {
+        loadTweets();
+      })
+      .fail((error) => console.error(error))
+    }
+  });
+
+  $('.compose').on('click', function(){
+    $('.new-tweet').slideToggle(80, function() {
+      if ($('.new-tweet').display !== "none") {
+        $('textarea').focus();
+      }
+    });
+  });
+});
+
+// renderTweets(data);
