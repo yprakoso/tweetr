3616f15213b15ff65e72d64f252b7d74cf425297
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-26 20:08:55 +0000

    install mongodb

---
+++ mongo_example.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  // ==> We have a connection to the "test-tweets" db,
+  //     starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  function getTweets(callback){
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet: ");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+  });
+});
